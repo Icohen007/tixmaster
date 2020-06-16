3ce13f35f1d1726d31f0974d4be0fd498c0392f7
Icohen007 <37484485+Icohen007@users.noreply.github.com>
2020-06-17 01:30:54 +0300

    Ingress Setup + dummy route. change ts start script.

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,7 +1,12 @@
 import express from 'express';
+import ROUTES from "./routes";
 
 const app = express();
 app.use(express.json());
+
+app.get(ROUTES.CURRENT_USER, (req, res) => {
+    res.send('route test');
+})
 
 app.listen(3000, () => {
     console.log('Listening on port 3000');
---
+++ auth/src/routes.ts
@@ -0,0 +1,5 @@
+const ROUTES = Object.freeze({
+    CURRENT_USER: '/api/users/currentuser'
+});
+
+export default ROUTES;
