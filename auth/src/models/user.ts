c7d48bd1744551162bbf5d73b94048b7595e3dcc
Icohen007 <37484485+Icohen007@users.noreply.github.com>
2020-06-18 21:20:44 +0300

    define mongoose Schema for user, has compatibility with Typescript

---
+++ auth/src/models/user.ts
@@ -0,0 +1,32 @@
+import mongoose from 'mongoose';
+
+interface UserAttributes {
+  email: string;
+  password: string;
+}
+
+interface UserModel extends mongoose.Model<UserDoc> {
+  build(attributes: UserAttributes): UserDoc;
+}
+
+interface UserDoc extends mongoose.Document {
+  email: string;
+  password: string;
+}
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+const User = mongoose.model<UserDoc, UserModel>('User', userSchema);
+
+userSchema.statics.build = (attributes: UserAttributes) => new User(attributes);
+
+export default User;
