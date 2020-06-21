fdee90439fb569f436bb7c9b2cfa4208c1152be2
Icohen007 <37484485+Icohen007@users.noreply.github.com>
2020-06-21 23:03:39 +0300

    extract signin to as a global function for test env.

--- auth/src/routes/__test__/current-user.test.ts
+++ auth/src/routes/__test__/current-user.test.ts
@@ -1,16 +1,8 @@
 import request from 'supertest';
 import app from '../../app';
 
-it('should respose with current user', async () => {
-  const signupResponse = await request(app)
-    .post('/api/users/signup')
-    .send({
-      email: 'test@test.com',
-      password: 'password',
-    })
-    .expect(201);
-
-  const cookie = signupResponse.get('Set-Cookie');
+it('should response with current user', async () => {
+  const cookie = await global.signin();
 
   const response = await request(app)
     .get('/api/users/currentuser')
@@ -20,4 +12,13 @@ it('should respose with current user', async () => {
 
   const { currentUser } = response.body;
   expect(currentUser.email).toEqual('test@test.com');
+});
+
+it('should response with null when not authenticated', async () => {
+  const response = await request(app)
+    .get('/api/users/currentuser')
+    .send()
+    .expect(200);
+
+  expect(response.body.currentUser).toBe(null);
 });
