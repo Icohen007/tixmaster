6625aa5b469f19aa04a18f3aba97dbdc9b6ae2ec
Icohen007 <37484485+Icohen007@users.noreply.github.com>
2020-06-24 02:09:23 +0300

    add axiosFactory which handlers: 1. internal routing DNS resolution in K8s on server side. 2. cookies header. 3. host header

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,1 +1,17 @@
-export default () => <h1>Landing Page</h1>;
+import axiosFactory from '../api/axiosFactory';
+
+const LandingPage = ({ currentUser }) => (
+  currentUser ? (
+    <h1>You are signed in</h1>
+  ) : (
+    <h1>You are NOT signed in</h1>
+  ));
+
+export async function getServerSideProps(context) {
+  const axios = axiosFactory(context);
+  const { data } = await axios.get('/api/users/currentuser');
+
+  return { props: data };
+}
+
+export default LandingPage;
