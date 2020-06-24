020da26cc07537abb9ece6e50aa9df7770bf7ade
Icohen007 <37484485+Icohen007@users.noreply.github.com>
2020-06-24 16:57:42 +0300

    move getInitialProps to app

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,3 +1,29 @@
 import 'bootstrap/dist/css/bootstrap.css';
+import axiosFactory from '../api/axiosFactory';
 
-export default ({ Component, pageProps }) => <Component {...pageProps} />;
+const AppComponent = ({ Component, pageProps, currentUser }) => (
+  <div>
+    <h1>
+      Header!
+      {currentUser.email}
+    </h1>
+    <Component {...pageProps} />
+  </div>
+);
+
+AppComponent.getInitialProps = async ({ Component, ctx }) => {
+  const client = axiosFactory(ctx);
+  const { data } = await client.get('/api/users/currentuser');
+
+  let pageProps = {};
+  if (Component.getInitialProps) {
+    pageProps = await Component.getInitialProps(ctx);
+  }
+
+  return {
+    pageProps,
+    ...data,
+  };
+};
+
+export default AppComponent;
--- client/pages/index.js
+++ client/pages/index.js
@@ -1,17 +1,8 @@
-import axiosFactory from '../api/axiosFactory';
-
 const LandingPage = ({ currentUser }) => (
   currentUser ? (
     <h1>You are signed in</h1>
   ) : (
     <h1>You are NOT signed in</h1>
   ));
-
-export async function getServerSideProps(context) {
-  const axios = axiosFactory(context);
-  const { data } = await axios.get('/api/users/currentuser');
-
-  return { props: data };
-}
 
 export default LandingPage;
