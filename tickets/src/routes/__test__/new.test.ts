991a746624cef866ca529ecb798ae53179104ba8
Itamar Cohen <you@example.com>
2020-06-27 23:31:21 +0300

    write tests first, TDD style

---
+++ tickets/src/routes/__test__/new.test.ts
@@ -0,0 +1,22 @@
+import request from 'supertest';
+import app from '../../app';
+
+it('has route handler listening to /api/tickets', async () => {
+
+});
+
+it('user should be signed in', async () => {
+
+});
+
+it('when invalid title, returns error', async () => {
+
+});
+
+it('when invalid price, returns error', async () => {
+
+});
+
+it('when vaild parameters, create ticket', async () => {
+
+});
