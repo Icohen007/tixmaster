ce0f707bd8d8f603290dca2358617ec97bf08296
Itamar Cohen <you@example.com>
2020-07-05 00:43:41 +0300

    add NatsWrapper Singelton and connect it.

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -1,5 +1,6 @@
 import mongoose from 'mongoose';
 import app from './app';
+import natsWrapper from './NatsWrapper';
 
 const initDb = async () => {
   if (!process.env.JWT_SECRET) {
@@ -11,6 +12,7 @@ const initDb = async () => {
   }
 
   try {
+    await natsWrapper.connect('tixmaster', 'client1', 'http://nats-srv:4222');
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
