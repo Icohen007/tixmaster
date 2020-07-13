95432e660dbb0f3a5dbb2345b656e2bcb9ff290b
Itamar Cohen <you@example.com>
2020-07-13 11:17:55 +0300

    move NATS hardcoded variables into variable envs

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -6,13 +6,23 @@ const initDb = async () => {
   if (!process.env.JWT_SECRET) {
     throw new Error('JWT_SECRET must be defined');
   }
-
   if (!process.env.MONGO_URI) {
     throw new Error('MONGO_URI must be defined');
   }
+  if (!process.env.NATS_CLIENT_ID) {
+    throw new Error('NATS_CLIENT_ID must be defined');
+  }
+  if (!process.env.NATS_URL) {
+    throw new Error('NATS_URL must be defined');
+  }
+  if (!process.env.NATS_CLUSTER_ID) {
+    throw new Error('NATS_CLUSTER_ID must be defined');
+  }
 
   try {
-    await natsWrapper.connect('tixmaster', 'client1', 'http://nats-srv:4222');
+    await natsWrapper.connect(process.env.NATS_CLUSTER_ID,
+      process.env.NATS_CLIENT_ID,
+      process.env.NATS_URL);
     natsWrapper.client.on('close', () => {
       console.log('NATS connection closed!');
       process.exit();
