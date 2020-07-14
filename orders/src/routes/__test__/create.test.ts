be5200663e9d73d9f74b4274674484a32b92332f
Itamar Cohen <you@example.com>
2020-07-14 17:21:29 +0300

    add tests for create route.

---
+++ orders/src/routes/__test__/create.test.ts
@@ -0,0 +1,60 @@
+import { OrderStatus } from '@tixmaster/common';
+import request from 'supertest';
+import { createOrder, generateMongooseId } from '../../test/helpers';
+import Ticket from '../../models/Ticket';
+import Order from '../../models/Order';
+import app from '../../app';
+
+it('when user is not signed in, returns 401', async () => {
+  await request(app)
+    .post('/api/orders')
+    .send({})
+    .expect(401);
+});
+
+it('when empty ticketId, returns error', async () => {
+  const emptyTicketId = { ticketId: '' };
+
+  await createOrder(emptyTicketId).expect(400);
+});
+
+it('when not mongoose ticketId, returns error', async () => {
+  const emptyTicketId = { ticketId: '123' };
+
+  await createOrder(emptyTicketId).expect(400);
+});
+
+it('when ticket not exist, returns 404', async () => {
+  await createOrder({ ticketId: generateMongooseId() })
+    .expect(404);
+});
+
+it('when ticket already reserved, returns 400', async () => {
+  const ticketParams = { title: 'title', price: 10 };
+
+  const ticket = Ticket.build(ticketParams);
+  await ticket.save();
+
+  const orderParams = {
+    ticket,
+    userId: 'userId',
+    status: OrderStatus.Created,
+    expiresAt: new Date(),
+  };
+
+  const order = Order.build(orderParams);
+  await order.save();
+
+  await createOrder({ ticketId: ticket.id }).expect(400);
+});
+
+it('when correct parameters, order created and ticket reserved', async () => {
+  const ticketParams = { title: 'title', price: 10 };
+
+  const ticket = Ticket.build(ticketParams);
+  await ticket.save();
+
+  const response = await createOrder({ ticketId: ticket.id }).expect(201);
+  expect(response.body.status).toEqual(OrderStatus.Created);
+  expect(response.body.ticket.id).toEqual(ticket.id);
+});
--- orders/src/test/helpers.ts
+++ orders/src/test/helpers.ts
@@ -1,3 +1,12 @@
 import mongoose from 'mongoose';
+import request from 'supertest';
+import app from '../app';
 
 export const generateMongooseId = () => new mongoose.Types.ObjectId().toHexString();
+
+export const createOrder = (orderParams: { ticketId: string }, cookie?: string[]) => (
+  request(app)
+    .post('/api/orders')
+    .set('Cookie', cookie || global.signin())
+    .send(orderParams)
+);
