9c9c4f65dc47a2379bcbfa0507c1daa03f0c4eb7
Itamar Cohen <you@example.com>
2020-07-14 16:04:07 +0300

    add Order create route. * find ticket check if it is in DB, and check if it reserved. * add new order to DB with generated expiration date.

--- orders/src/routes/create.ts
+++ orders/src/routes/create.ts
@@ -1,7 +1,14 @@
 import express, { Request, Response } from 'express';
-import { requireAuth, validateRequest } from '@tixmaster/common';
+import {
+  BadRequestError,
+  NotFoundError, OrderStatus,
+  requireAuth,
+  validateRequest,
+} from '@tixmaster/common';
 import { body } from 'express-validator';
 import mongoose from 'mongoose';
+import Ticket from '../models/Ticket';
+import Order from '../models/Order';
 
 const router = express.Router();
 
@@ -12,8 +19,35 @@ const validationChains = [
     .withMessage('You must supply valid ticketId'),
 ];
 
+const EXPIRATION_SECONDS = 15 * 60;
+
+function generateExpirationDate() {
+  const expiresAt = new Date();
+  expiresAt.setSeconds(expiresAt.getSeconds() + EXPIRATION_SECONDS);
+  return expiresAt;
+}
+
 router.post('/api/orders', requireAuth, validationChains, validateRequest, async (req: Request, res: Response) => {
-  res.status(200).send({});
+  const ticket = await Ticket.findById(req.body.ticketId);
+  if (!ticket) {
+    throw new NotFoundError();
+  }
+
+  const isReserved = await ticket.isReserved();
+  if (isReserved) {
+    throw new BadRequestError('Ticket is already reserved');
+  }
+
+  const newOrder = Order.build({
+    userId: req.currentUser!.id,
+    status: OrderStatus.Created,
+    expiresAt: generateExpirationDate(),
+    ticket,
+  });
+
+  await newOrder.save();
+
+  res.status(201).send(newOrder);
 });
 
 export default router;
