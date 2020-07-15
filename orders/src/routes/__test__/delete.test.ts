71d1b43a5a0bd6a699ee88a928ab23106f343d26
Itamar Cohen <you@example.com>
2020-07-15 14:11:16 +0300

    add Orders service delete route + tests.

---
+++ orders/src/routes/__test__/delete.test.ts
@@ -0,0 +1,56 @@
+import request from 'supertest';
+import { OrderStatus } from '@tixmaster/common';
+import app from '../../app';
+import { createOrder, generateMongooseId, insertTicketToDb } from '../../test/helpers';
+
+it('when user is not authenticated, return 401', async () => {
+  await request(app)
+    .delete(`/api/orders/${generateMongooseId()}`)
+    .send()
+    .expect(401);
+});
+
+it('when order is not exist, return 404', async () => {
+  const cookie = global.signin();
+
+  await request(app)
+    .delete(`/api/orders/${generateMongooseId()}`)
+    .set('Cookie', cookie)
+    .send()
+    .expect(404);
+});
+
+it('when one user try to fetch another user order, returns 401', async () => {
+  const ticket = await insertTicketToDb();
+  const cookie1 = global.signin();
+  const { body: order } = await createOrder({ ticketId: ticket.id }, cookie1).expect(201);
+
+  const cookie2 = global.signin();
+  await request(app)
+    .delete(`/api/orders/${order.id}`)
+    .set('Cookie', cookie2)
+    .send()
+    .expect(401);
+});
+
+it('when order is exist, fetch the order', async () => {
+  const ticket = await insertTicketToDb();
+  const cookie = global.signin();
+  const { body: order } = await createOrder({ ticketId: ticket.id }, cookie).expect(201);
+
+  await request(app)
+    .delete(`/api/orders/${order.id}`)
+    .set('Cookie', cookie)
+    .send()
+    .expect(204);
+
+  const { body: updatedOrder } = await request(app)
+    .get(`/api/orders/${order.id}`)
+    .set('Cookie', cookie)
+    .send()
+    .expect(200);
+
+  expect(updatedOrder.status).toEqual(OrderStatus.Cancelled);
+});
+
+it.todo('emits an order cancelled event');
